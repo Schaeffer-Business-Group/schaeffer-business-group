5603ff30b453d94e96afbc048939f6f014215852
blaise2s <blaise.schaeffer@gmail.com>
2021-04-11 15:52:35 -0400

    feat: configures travis and ci integrations

---
+++ app-server.ts
@@ -0,0 +1,23 @@
+import * as Express from 'express';
+import * as Path from 'path';
+
+const app = Express();
+
+app.use(Express.static(Path.join(__dirname, 'schaeffer-business-group-site')));
+
+app.get('/*', (_request, response) => {
+  response.sendFile(
+    Path.join(__dirname, 'schaeffer-business-group-site', 'index.html')
+  );
+});
+
+const port = process.env.PORT ? +process.env.PORT : 4200;
+const host = process.env.HOST;
+const callbackFn = (p: number) => {
+  console.log(`Schaeffer Business Group 🚀 app started on port ${p}`);
+};
+console.log(`HOST: ${host ? host : 'env variable is not set'}`);
+console.log(`PORT: ${port ? port : 'env variable is not set'}`);
+host
+  ? app.listen(port, host, () => callbackFn(port))
+  : app.listen(port, () => callbackFn(port));
